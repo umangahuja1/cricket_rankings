2d3c4ce02b050a28691a49f936f48078e7ee1790
umangahuja1 <umangahuja1@gmail.com>
2018-06-24 12:52:18 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cricket_rankings',
+      version='0.1',
+      description='Cricket ranking of teams and players across all formats for both men and women',
+      url='http://github.com/umangahuja1/cricket_rankings',
+      author='Umang Ahuja',
+      author_email='umangahuja1@gmail.com',
+      license='MIT',
+      packages=['cricket_rankings'],
+      zip_safe=False)
